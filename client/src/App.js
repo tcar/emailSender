4c2c690d070b329adf8d37cd6c63ea9c20942191
tcar <tihomir.car@uniqcast.com>
2020-01-06 10:53:55 +0100

    lint code
    Signed-off-by: tcar <tihomir.car@uniqcast.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import Main from "./containers/main"
-import { BrowserRouter as Router} from "react-router-dom";
-import { Grid } from '@material-ui/core';
+import Main from "./containers/main";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
-export default class App extends Component{
-    render(){
-        return(
-                <Grid
-                container
-                justify="center"
-                >
-                    <Grid item xs={9}>
-                        <Main />
-                    </Grid>
-                </Grid>
-        )
-    }
+export default class App extends Component {
+  render() {
+    return (
+      <Grid container justify="center">
+        <Grid item xs={9}>
+          <Main />
+        </Grid>
+      </Grid>
+    );
+  }
 }
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-
-export default class Home extends Component{
-    render(){
-        return(
-            <div>
-                <h1>
-                    Home
-                </h1>
-            </div>
-        )
-    }
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
 }
